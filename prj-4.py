501d2212c6f8b562f83a99cf5f7069919c16a8b2
Robin Yadav <48661780+robinyUArizona@users.noreply.github.com>
2021-12-02 21:54:31 -0700

    Add files via upload

---
+++ prj-4.py
@@ -0,0 +1,10 @@
+
+
+spark = SparkSession.builder.appName("P4Trends").getOrCreate()
+
+
+movies1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/movies.dat").withColumnRenamed("_c0", "movieID").withColumnRenamed("_c1", "MovieName").withColumnRenamed("_c2", "Genre")
+
+ratings1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/ratings.dat").withColumnRenamed("_c0", "userID").withColumnRenamed("_c1", "movieID").withColumnRenamed("_c2", "Rating").withColumnRenamed("_c3", "Timestamp")
+
+users1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/users.dat").withColumnRenamed("_c0", "userID").withColumnRenamed("_c1", "Gender").withColumnRenamed("_c2", "Age").withColumnRenamed("_c3", "Occupation").withColumnRenamed("_c4", "Zip-Code")
