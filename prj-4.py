26999607c03ca7debc66151cb7e09af57f6ca3a2
Robin Yadav <48661780+robinyUArizona@users.noreply.github.com>
2021-12-03 17:48:45 -0700

    Add files via upload

--- prj-4.py
+++ prj-4.py
@@ -1,10 +1,27 @@
 
 
+from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("P4Trends").getOrCreate()
 
 
-movies1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/movies.dat").withColumnRenamed("_c0", "movieID").withColumnRenamed("_c1", "MovieName").withColumnRenamed("_c2", "Genre")
 
-ratings1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/ratings.dat").withColumnRenamed("_c0", "userID").withColumnRenamed("_c1", "movieID").withColumnRenamed("_c2", "Rating").withColumnRenamed("_c3", "Timestamp")
+movies1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/movies.dat") \
+    .withColumnRenamed("_c0", "movieID") \
+    .withColumnRenamed("_c1", "MovieName") \
+    .withColumnRenamed("_c2", "Genre")
 
-users1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/users.dat").withColumnRenamed("_c0", "userID").withColumnRenamed("_c1", "Gender").withColumnRenamed("_c2", "Age").withColumnRenamed("_c3", "Occupation").withColumnRenamed("_c4", "Zip-Code")
+ratings1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/ratings.dat")\
+    .withColumnRenamed("_c0", "userID") \
+    .withColumnRenamed("_c1", "movieID") \
+    .withColumnRenamed("_c2", "Rating") \
+    .withColumnRenamed("_c3", "Timestamp")
+
+users1M = spark.read.option("delimiter", "::").csv("movies-data/ml-1m/users.dat")\
+    .withColumnRenamed("_c0", "userID") \
+    .withColumnRenamed("_c1", "Gender") \
+    .withColumnRenamed("_c2", "Age") \
+    .withColumnRenamed("_c3", "Occupation") \
+    .withColumnRenamed("_c4", "Zip-Code")
+
+
+
