2c745958e9e03b895e284979ad94d61fec11c6d8
Robin Yadav <48661780+robinyUArizona@users.noreply.github.com>
2021-12-05 08:32:04 -0700

    Add files via upload

--- prj-4.py
+++ prj-4.py
@@ -8,7 +8,7 @@
 from pyspark import sql
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-from pyspark.sql.types import IntegerType,BooleanType,DateType, StringType
+from pyspark.sql.types import IntegerType, BooleanType, DateType, StringType
 
 
 spark = SparkSession.builder.appName("MoviesDataMining").getOrCreate()
@@ -23,9 +23,9 @@ movies1M = spark.read.option("delimiter", "::").csv(movies_file_location) \
     .withColumnRenamed("_c1", "MovieName") \
     .withColumnRenamed("_c2", "Genre")
 
-movies1M = movies1M.withColumn("movieID", movies1M.movieID.cast('integer'))
+movies1M = movies1M.withColumn("movieID", movies1M.movieID.cast(IntegerType()))
 # movies1M.show(5)
-movies1M.printSchema()
+# movies1M.printSchema()
 
 
 ratings1M = spark.read.option("delimiter", "::").csv(ratings_file_location)\
@@ -34,13 +34,13 @@ ratings1M = spark.read.option("delimiter", "::").csv(ratings_file_location)\
     .withColumnRenamed("_c2", "Rating") \
     .withColumnRenamed("_c3", "Timestamp")
 
+from pyspark.sql import functions as F
+ratings1M = ratings1M.withColumn("userID", col("userID").cast(IntegerType())) \
+        .withColumn("movieID", col("movieID").cast(IntegerType())) \
+        .withColumn("Rating", col("Rating").cast(IntegerType())) \
+        .withColumn("Timestamp", F.col('Timestamp').cast("timestamp"))
 
-ratings1M = ratings1M.withColumn("userID", col("userID").cast('integer')) \
-        .withColumn("movieID", col("movieID").cast("integer")) \
-        .withColumn("Rating", col("Rating").cast("integer")) \
-        .withColumn("Timestamp", col("Timestamp"))
-
-# ratings1M.show(5)
+ratings1M.show(5)
 ratings1M.printSchema()
 
 
@@ -51,36 +51,48 @@ users1M = spark.read.option("delimiter", "::").csv(users_file_location)\
     .withColumnRenamed("_c3", "Occupation") \
     .withColumnRenamed("_c4", "Zip-Code")
 
-users1M = users1M.withColumn("userID", col("userID").cast('integer')) \
+users1M = users1M.withColumn("userID", col("userID").cast(IntegerType())) \
         .withColumn("Gender", col("Gender").cast(StringType())) \
-        .withColumn("Age", col("Age").cast("integer")) \
+        .withColumn("Age", col("Age").cast(IntegerType())) \
         .withColumn("Occupation", col("Occupation").cast(StringType())) \
         .withColumn("Zip-Code", col("Zip-Code").cast(StringType()))
 # users1M.show(5)
-users1M.printSchema()
+# users1M.printSchema()
 
 
 
 
 ## Join movies and ratings, key is movieID
-movies_ratings = movies1M.join(ratings1M, movies1M.movieID == ratings1M.movieID, "outer")
-movies_ratings.show(truncate=False)
+movies_ratings = movies1M.join(ratings1M, movies1M.movieID == ratings1M.movieID, "outer") \
+    .drop(ratings1M.movieID)
+# movies_ratings.show(truncate=False)
 
 
 ## Join ratings and users, key is userID
-users_ratings = users1M.join(ratings1M, users1M.userID == ratings1M.userID, "outer")
-users_ratings.show(truncate=False)
+users_ratings = users1M.join(ratings1M, users1M.userID == ratings1M.userID, "outer") \
+    .drop(ratings1M.userID)
+# users_ratings.show(truncate=False)
 
 
 # What are k most popular movies of all time?
-movies_ratings_avg = movies_ratings.groupby(['movieID', 'MovieName']).avg('Rating') \
-    .alias("Avg Rating")
-movies_ratings_avg.show()
-
-
+movies_ratings_avg = movies_ratings.groupBy(['movieID', 'MovieName'])\
+    .avg('Rating')\
+    .sort(col("avg(Rating)") \
+    .desc()
+)
+# print("==== k most popular movies of all time ====")
+# movies_ratings_avg.show()
 
 
 # What are k most popular movies for a particular year?
+# from pyspark.sql import functions as f
+# from pyspark.sql import types as t
+# movies_ratings = movies_ratings.withColumn('Timestamp', \
+#     f.to_date(movies_ratings.Timestamp.cast(dataType=t.TimestampType())))
+# movies_ratings.show()
+# movies_ratings.printSchema()
+
+
 # What are k most popular movies for a certain age group?
 # What are k most popular movies for particular season (summer, fall, winter, spring)?
 # What are the top k movies with the most ratings (presumably most popular) that have the lowest ratings?
