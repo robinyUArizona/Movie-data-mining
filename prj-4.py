b865100e86211e7e46bcc4c9e3b90f15794cc04d
Robin Yadav <48661780+robinyUArizona@users.noreply.github.com>
2021-12-05 19:48:17 -0700

    Add files via upload

--- prj-4.py
+++ prj-4.py
@@ -4,11 +4,14 @@
 # Reference: https://www.ijrte.org/wp-content/uploads/papers/v7i5s2/ES2097017519.pdf
 # Reference: 
 
-
+import time
+import datetime
 from pyspark import sql
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.types import IntegerType, BooleanType, DateType, StringType
+from pyspark.sql.functions import lit, unix_timestamp
+from pyspark.sql.functions import year, month, dayofmonth
 
 
 spark = SparkSession.builder.appName("MoviesDataMining").getOrCreate()
@@ -34,14 +37,17 @@ ratings1M = spark.read.option("delimiter", "::").csv(ratings_file_location)\
     .withColumnRenamed("_c2", "Rating") \
     .withColumnRenamed("_c3", "Timestamp")
 
-from pyspark.sql import functions as F
+
+
+timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
 ratings1M = ratings1M.withColumn("userID", col("userID").cast(IntegerType())) \
         .withColumn("movieID", col("movieID").cast(IntegerType())) \
         .withColumn("Rating", col("Rating").cast(IntegerType())) \
-        .withColumn("Timestamp", F.col('Timestamp').cast("timestamp"))
+        .withColumn("Timestamp", unix_timestamp(lit(timestamp),'yyyy-MM-dd HH:mm:ss').cast("timestamp"))
 
-ratings1M.show(5)
-ratings1M.printSchema()
+# ratings1M.show(5)
+# ratings1M.printSchema()
 
 
 users1M = spark.read.option("delimiter", "::").csv(users_file_location)\
@@ -78,22 +84,46 @@ users_ratings = users1M.join(ratings1M, users1M.userID == ratings1M.userID, "out
 movies_ratings_avg = movies_ratings.groupBy(['movieID', 'MovieName'])\
     .avg('Rating')\
     .sort(col("avg(Rating)") \
-    .desc()
-)
-# print("==== k most popular movies of all time ====")
+    .desc())
+print("==== k most popular movies of all time ====")
 # movies_ratings_avg.show()
 
 
 # What are k most popular movies for a particular year?
-# from pyspark.sql import functions as f
-# from pyspark.sql import types as t
-# movies_ratings = movies_ratings.withColumn('Timestamp', \
-#     f.to_date(movies_ratings.Timestamp.cast(dataType=t.TimestampType())))
-# movies_ratings.show()
-# movies_ratings.printSchema()
+movies_ratings = movies_ratings.withColumn("Year", year(col("Timestamp")))
+
+print("==== k most popular movies for a particular year ====")
+movies_ratings_year = movies_ratings.groupBy(['Year', 'movieID', 'MovieName']) \
+    .avg('Rating')\
+    .sort(col("avg(Rating)") \
+    .desc())
+
+# movies_ratings_year.show()
+
+movies_ratings_year_2021 = movies_ratings_year[movies_ratings_year["Year"] == 2021]
+# movies_ratings_year_2021.show()
 
 
 # What are k most popular movies for a certain age group?
+users_ratings_movies = users_ratings.join(movies1M, users_ratings.movieID == movies1M.movieID, \
+    "outer").drop(movies1M.movieID)
+# users_ratings_movies.show()
+
+print("==== k most popular movies for a certain age group ====")
+movies_ratings_age = users_ratings_movies.groupBy(['Age', 'movieID', 'MovieName']) \
+    .avg('Rating')\
+    .sort(col("avg(Rating)") \
+    .desc())
+
+# movies_ratings_age.show()
+
+movies_ratings_age_25 = movies_ratings_age[movies_ratings_age["Age"] == 25]
+movies_ratings_age_25.show()
+
+
+
+
+
 # What are k most popular movies for particular season (summer, fall, winter, spring)?
 # What are the top k movies with the most ratings (presumably most popular) that have the lowest ratings?
 
